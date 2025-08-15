c866c06b023d5453357a4d8ead7d3a2648f15bae
delozath <delozath@gmail.com>
2025-08-15 14:33:59 -0600

    testing report tool for dictionary

---
+++ mlops/reports/__init__.py
@@ -0,0 +1,5 @@
+from ntxter.mlops.reports.__reports import MetricsTableDriver
+
+__all__ = [
+    'MetricsTableDriver'
+]
--- mlops/reports/__reports.py
+++ mlops/reports/__reports.py
@@ -1,42 +1,21 @@
 import pandas as pd
 
-class DataFramePerformanceIterative:
+class MetricsTableDriver:
     def __init__(self, cols):
-        self.columns = cols
-        self._table = []
+        self.cols = cols
         self._row = None
+        self._table = []
     #
     @property
     def row(self):
-        if self._row:
-            return self._row
-        else:
-            return None
+        return self._row
     #
     @row.setter
     def row(self, value):
-        if self._row is None:
-            self._row = value
-        else:
-            raise RuntimeError(f"current row has not been append to the report table")
-    #
-    @property
-    def table(self):
-        if len(self._table)>0:
-            return pd.concat(self._table)
-        else:
-            return
-    #
-    def append_to_row(self, values):
-        if self._row is not None:
-            self._row |= values
-        else:
-            raise RuntimeError(f"current row has not been inicialized")
+        self._row = self._row | value if self._row else value
     #
     def append(self):
-        if self._row is not None:
-            tmp = pd.DataFrame([self._row])
-            self._table.append(tmp)
-            self._row = None
-        else:
-            print("No row to append. Continue")
+        self._table.append(self._row)
+    #
+    def generate(self):
+        return pd.DataFrame(self._table)
