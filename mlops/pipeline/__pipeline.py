0d0ee2a45803e6674afe2ee747ac53ac098f434d
delozath <delozath@gmail.com>
2025-09-22 23:20:26 -0600

    refactor: register Pipelines migrated to a simplest way using no decorators

--- mlops/pipeline/__pipeline.py
+++ mlops/pipeline/__pipeline.py
@@ -33,25 +33,22 @@ class PipelineFactory:
     def __init__(self) -> None:
         self._registry = {}
 
-    def register(self, key: str, info: dict):
-        def deco(fn):
+    def register(self, def_pipe, descriptor, model_params, id=None):
+            key = (
+                id if id else f"{def_pipe.__name__}: {descriptor['outcome']} ~ {descriptor['model']}"
+            )
             if key in self._registry:
                 raise DuplicateKeyError(f"Attempt to register the key '{key}' that is already registered model", 100)
-            if isinstance(info, dict):
-                raise TypeError("info parameter must be dict type")
-            reg = info | {'pipeline': fn}
+            inst = def_pipe()
+            inst.build(**model_params)
+            reg = {'pipe_wrap': inst}
+            reg |= descriptor
             self._registry[key] = reg
-            return fn
-        return deco
-    
-    def build(self, key: str, *args, **kwargs):
-        model = self._registry[key](*args, **kwargs)
-        return model
-    
+
     def __repr__(self):
         output = f"{type(self).__name__} registered:\n"
         output += '\n'.join(
-            [f"{k}, {i.__name__}" for k, i in self._registry.items()]
+            [f"{k}, {i}" for k, i in self._registry.items()]
          )
         output += '\n'
         return output
