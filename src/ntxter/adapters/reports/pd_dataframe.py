0e27aa6dc4dfa7df32799e53dac1de3ffc1c371c
delozath <delozath@gmail.com>
2025-11-21 12:47:08 -0600

    feat:  - edits to pd_dataframe report adapter to fix saving issue  - add markdown report adapter for pandas dataframes
    refactor:
     - edits to pd_dataframe api to include simpler report generation without grouping

--- src/ntxter/adapters/reports/pd_dataframe.py
+++ src/ntxter/adapters/reports/pd_dataframe.py
@@ -6,10 +6,29 @@ import pandas as pd
 
 from ntxter.core.reports.dataframes import BaseDataFrameReport
 
+def base_save(pfname: Path, md_report: str) -> None:
+    if not isinstance(pfname, Path):
+        pfname = Path(pfname)
+    
+    pfname.parent.mkdir(parents=True, exist_ok=True)
+    pfname.write_text(md_report)
+    
+
+class ReportIntoMD(BaseDataFrameReport[pd.DataFrame, str]):
+    def build(self, df: pd.DataFrame, /, **kwargs) -> str:
+        report = f"# Pandas DataFrame Report\n\n"
+        report += df.to_markdown()
+        return report
+
+    def save(self, pfname: Path, /, *, df: pd.DataFrame, **kwargs) -> None | str:
+        md_report = self.build(df)
+        base_save(pfname, md_report)
+        return md_report
+
 
 class ReportByGroupIntoMD(BaseDataFrameReport[pd.DataFrame, str]):
     def build(self, df: pd.DataFrame, /, *, grouping: str | List[str], **kwargs) -> str:
-        report = f"# Grouping Report {grouping}\n\n"
+        report = f"# Pandas DataFrame Reporting by Groups: {grouping}\n\n"
         for name, grp in df.groupby(grouping):
             report += f"## Group: {name}\n"
             report += grp.drop(columns=grouping).to_markdown() + "\n\n"
@@ -18,10 +37,5 @@ class ReportByGroupIntoMD(BaseDataFrameReport[pd.DataFrame, str]):
 
     def save(self, pfname: Path, /, *, df: pd.DataFrame, grouping: str | List[str], **kwargs) -> None | str:
         md_report = self.build(df, grouping=grouping)
-        if not isinstance(pfname, Path):
-            pfname = Path(pfname)
-        else:
-            pfname.parent.mkdir(parents=True, exist_ok=True)
-            pfname.write_text(md_report)
-        
+        base_save(pfname, md_report)
         return md_report
--- src/ntxter/api/reports/pd_dataframes.py
+++ src/ntxter/api/reports/pd_dataframes.py
@@ -1,5 +1,10 @@
-from ntxter.adapters.reports.pd_dataframe import ReportByGroupIntoMD as ByGrouptIntoMD
+from ntxter.adapters.reports.pd_dataframe import (
+    ReportByGroupIntoMD as ByGrouptIntoMD,
+    ReportIntoMD as ToMarkdown
+ )
+
 
 __all__ = [
     "ByGrouptIntoMD",
+    "ToMarkdown",
 ]
