c8031063706f643f28ad99b85c717fe662d38e8a
delozath <delozath@gmail.com>
2025-12-14 21:18:29 -0600

    feat: enhance StatisticsNormalityPandasDF to support multiple normality tests and improve data handling

--- src/ntxter/adapters/statistics/base.py
+++ src/ntxter/adapters/statistics/base.py
@@ -47,13 +47,30 @@ class StatisticsNormalityPandasDF(StatisticsBase[pd.DataFrame, pd.DataFrame]):
 
     @override
     def compute(self, grouping: list[str] | str | None = None) -> Self | StatisticsContainer[pd.DataFrame]:
+        funcs = {
+            'shapiro': self._shapiro,
+            'dagostino': self._dagostino,
+            'anderson': self._anderson,
+            'ks': self._kolmogorov_smirnov,
+        }
+
         if grouping is not None:
             grouping = [grouping] if isinstance(grouping, str) else grouping
         
         if isinstance(self.data, pd.DataFrame):
+            Test = []
             for loop in self._query(self.data, grouping=grouping):
-                self._shapiro(loop[2])
-                breakpoint()
+                for test, func in funcs.items():
+                    df = self._to_frame(
+                        test,
+                        loop[0],
+                        loop[1],
+                        func(loop[2])
+                    )
+                    
+                    Test.append(df)
+            result_df = pd.concat(Test, ignore_index=True)
+            breakpoint()
         else:
             raise TypeError("Data must be a Pandas DataFrame.")
         
@@ -75,17 +92,68 @@ class StatisticsNormalityPandasDF(StatisticsBase[pd.DataFrame, pd.DataFrame]):
         disagg = self.disaggregate_to_df(x)
         cols = disagg.query("(dtype == 'numerical') & (ntype=='continuous')").index.tolist()
 
-        #if grouping is None:
-        #   # for name, col inx[cols]
-        #   yield from x[cols].items()
-        #else:
-        df = x[cols + grouping]
+        if grouping is not None:
+            df = x[cols + grouping]
+        else:
+            grouping = ['fake_group']
+            df = x[cols]
+            df['fake_group'] = 'all_data'
+        
         for name, group in df.groupby(grouping):
             group = group.drop(columns=grouping)
             for col, xf in group.items():
                 yield name, col, xf
     
+    def _to_frame(self, test, group, column, values) -> pd.DataFrame:
+        group = '' if group is None else '_'.join(map(str, group))
+
+        df = (pd.DataFrame
+            .from_dict(values, orient='index')
+            .reset_index()
+            .rename(columns={'index': 'params', 0: 'value'})
+         )
+        
+        df['test'] = test
+        df['column'] = column
+        df['group'] = group
+        
+        return df
+
     def _shapiro(self, x: pd.Series) -> Dict[str, float]:
         stat, p_value = shapiro(x.dropna())
-        breakpoint()
-        return {'shapiro_stat': stat, 'shapiro_p_value': p_value}
+        params = {
+            'stats': stat, 
+            'p_value': p_value
+        }
+        
+        return params
+    
+    def _dagostino(self, x: pd.Series) -> Dict[str, float]:
+        stat, p_value = normaltest(x.dropna())
+        params = {
+            'stats': stat, 
+            'p_value': p_value
+        }
+        
+        return params
+    
+    def _anderson(self, x: pd.Series) -> Dict[str, float]:
+        result = anderson(x.dropna(), dist='norm')
+        params = {
+            'stats': result.statistic, 
+            'critical_values': result.critical_values.tolist(),
+            'significance_level': result.significance_level.tolist()
+        }
+        
+        return params
+    
+    def _kolmogorov_smirnov(self, x: pd.Series) -> Dict[str, float]:
+        mean = np.mean(x.dropna())
+        std = np.std(x.dropna())
+        stat, p_value = kstest(x.dropna(), 'norm', args=(mean, std))
+        params = {
+            'stats': stat, 
+            'p_value': p_value
+        }
+        
+        return params
