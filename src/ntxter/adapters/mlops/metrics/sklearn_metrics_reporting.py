0751eddb146bd5b23f54d58e356252d9b85ecd06
delozath <delozath@gmail.com>
2025-12-22 23:50:27 -0600

    feat: enhance SklearnMetricsReporting to build metrics DataFrame and calculate median IQR for performance metrics

--- src/ntxter/adapters/mlops/metrics/sklearn_metrics_reporting.py
+++ src/ntxter/adapters/mlops/metrics/sklearn_metrics_reporting.py
@@ -38,7 +38,9 @@ class SklearnMetricsReporting(BaseReportMetrics[float]):
     
     @override
     def build(self) -> pd.DataFrame:
+        metrics = set(self._registry[0].performances.keys())
         records = []
+        append = records.append
         for report in self._registry.values():
             record = {
                 'identifier': report.identifier,
@@ -46,7 +48,32 @@ class SklearnMetricsReporting(BaseReportMetrics[float]):
                 **report.performances,
                 **(report.optional if report.optional is not None else {})
             }
-            records.append(record)
+            append(record)
+            metrics = metrics.union(report.performances.keys())
+        
+        self.metrics = list(metrics)
         
         return pd.DataFrame(records)
+    
+    def build_table_medians(self, df_metrics: pd.DataFrame, groupby: list[str] | str) -> pd.DataFrame:
+        cols = self.metrics + [groupby] if isinstance(groupby, str) else self.metrics + groupby
+        table = df_metrics[cols].melt(id_vars=groupby)
+        table.pivot_table(index=groupby, columns='variable', aggfunc=self._median_iqr)
+        table = (
+            df_metrics[cols]
+                .melt(id_vars=groupby)
+                .pivot_table(
+                    index=groupby,
+                    columns='variable',
+                    aggfunc=self._median_iqr
+                 )
+        )
+        return table
+    
+    def _median_iqr(self, series: pd.Series, dec: int = 4) -> str:
+        median = series.median()
+        q1 = series.quantile(0.25)
+        q3 = series.quantile(0.75)
+        
+        return f"{median:.{dec}f} ({q1:.{dec}f}--{q3:.{dec}f})"
 
